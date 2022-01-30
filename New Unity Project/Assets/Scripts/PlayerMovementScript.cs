893ca7d2d2be1f8099d9365717d43ad0be9bedb6
Rojash shahi <35806489+Rojash123@users.noreply.github.com>
2022-01-30 11:30:29 +0545

    Revert "female movement"
    This reverts commit f69816945d9e2f4f78720296d3e868389714656f.

--- New Unity Project/Assets/Scripts/PlayerMovementScript.cs
+++ New Unity Project/Assets/Scripts/PlayerMovementScript.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Player1MovementScript : MonoBehaviour
+public class PlayerMovementScript : MonoBehaviour
 {
     public CharacterController2D controller;
     public Animator animator;
@@ -12,10 +12,10 @@ public class Player1MovementScript : MonoBehaviour
 
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("HorizontalPlayer1") * runSpeed;
-
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("speed",Mathf.Abs(horizontalMove));
-        if(Input.GetKeyDown(KeyCode.W)){
+        Debug.Log(horizontalMove);
+        if(Input.GetButtonDown("Jump")){
             jump = true;
             StartCoroutine(AnimatorMakeJump());
         }
@@ -24,17 +24,20 @@ public class Player1MovementScript : MonoBehaviour
     
     void FixedUpdate()
     {
+        Debug.Log("call Fixed update");
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
     }
 
     public void OnLandPlayer(){
+        
         animator.SetBool("jump",false);
     }
 
     private IEnumerator AnimatorMakeJump()
     {
-        yield return new WaitForSeconds(0.04f);
+        
+        yield return new WaitForSeconds(0.04f);//because instantground check is not taken.
         animator.SetBool("jump",true);
     }
 }
